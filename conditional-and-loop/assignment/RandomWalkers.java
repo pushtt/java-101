3d1afd6fe3bcdebce6d1f775e891ce3ad97c2aa7
Thinh Tran <thinhphuctran@gmail.com>
2024-02-01 07:57:04 +0700

    assignment Week 1

---
+++ conditional-and-loop/assignment/RandomWalkers.java
@@ -0,0 +1,20 @@
+public class RandomWalkers {
+    public static void main(String[] args) {
+        int r = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        double step = 0;
+        for (int i = 0; i < trials; i++) {
+            int x = 0;
+            int y = 0;
+            while (Math.abs(x) + Math.abs(y) != r) {
+                step += 1;
+                double p = Math.random();
+                if (p < 0.25) x += 1;
+                else if (p < 0.50) x -= 1;
+                else if (p < 0.75) y += 1;
+                else if (p < 1.00) y -= 1;
+            }
+        }
+        System.out.println("average number of steps: " + step/trials);
+    }
+}
